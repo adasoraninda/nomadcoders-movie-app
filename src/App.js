2b1f2d4a8fdd3b7e4e5a3a134be79c452a95ff0f
adasoraninda <adasoraninda@gmail.com>
2021-06-14 16:22:00 +0700

    #2.1 Implement list item and prop types

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ fav }) {
-  return <h3>I like {fav}</h3>;
+const foods = [
+  {
+    id: 1,
+    name: "Burger",
+    image:
+      "https://image.freepik.com/free-psd/delicious-burger-food-menu-social-media-banner-template_106176-360.jpg",
+    rating: 2,
+  },
+  {
+    id: 2,
+    name: "Vegetables",
+    image:
+      "https://image.freepik.com/free-photo/basket-full-vegetables_1112-316.jpg",
+    rating: 3.4,
+  },
+];
+
+function renderFood(food) {
+  return (
+    <Food
+      key={food.id}
+      name={food.name}
+      picture={food.image}
+      rating={food.rating}
+    />
+  );
 }
 
-function App() {
+function Food({ name, picture, rating }) {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="Kimchi" />
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
     </div>
   );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+function App() {
+  return <div>{foods.map(renderFood)}</div>;
 }
 
 export default App;
